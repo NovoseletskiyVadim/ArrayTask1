dab6b2e31a6b9bbeea931d8222f7e6c766eba806
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2018-10-15 01:26:06 +0300

    Сorrection of syntax errors and  add method "pop"

--- array.js
+++ array.js
@@ -1,9 +1,22 @@
 //add new array
 var myMonth=new Array("июнь","июль","август");
-console.log(myMonth);
+console.log("Исходный массив:",myMonth);
 
 //add method "push"
-myMonth.push("сентябрь","ноябрь","октябрь");
-console.log(myMonth);
+myMonth.push("сентябрь","октябрь","ноябрь");
+console.log("работа метода \"push\":",myMonth);
+
+//add method "unshift"
+myMonth.unshift("март","апрель","май");
+console.log("работа метода \"unshift\":",myMonth);
+
+//add method "pop"
+var lastDeleteElemetArray_myMonth =myMonth.pop();
+console.log("работа метода \"pop\":")
+console.log("последний удаленный ел.массива:", lastDeleteElemetArray_myMonth);
+console.log("Исходный массив:",myMonth);
+
+
+
 
 
